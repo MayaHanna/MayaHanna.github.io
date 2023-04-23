1f84139c77e7002db67511333759794ae897e9b5
Maya Hanna <mayshanna.mh@gmail.com>
2023-04-23 12:42:42 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <img src="logo.jpg" className='logo'/>
+      <p className='title'>Reviews By Maya</p>
+      <div className='links'>
+        <a href='https://il.iherb.com/ugc/wishlist?id=7f10160d-83a1-4e80-b995-c9f80ed7adb3&rcode=BGG7944' className='link'>
+          רשימת ההמלצות שלי בiHerb ❤️‍🔥
         </a>
-      </header>
+        <a href='https://wa.me/message/PSFPDRJX5CLXP1'  className='link'>
+          שירות יעוץ ובניית שגרת טיפוח אישית 🧴
+        </a>
+        <a href='https://chat.whatsapp.com/DKAUWvQeXNbITi3STqkoAz'  className='link'>
+           התראות על חזרה למלאי 🛎️
+        </a>
+        <a href='https://chat.whatsapp.com/JdpjksFloHg8YNXD63lPAm'  className='link'>
+          קבוצת הווצאפ של העמוד 💬
+        </a>
+
+        <a href='https://il.iherb.com/ugc/wishlist?id=7f10160d-83a1-4e80-b995-c9f80ed7adb3&rcode=BGG7944'  className='link'>
+         קוד הנחה באייהרב- BGG7944 🛒
+        </a>
+        <a href='https://www.yesstyle.com/en/home.html?rco=YESMAYA23&utm_term=YESMAYA23&utm_medium=Influencer&utm_source=dynamic&mcg=influencer'  className='link'>
+        קוד הנחה ביס סטייל- YESMAYA23 🛒
+        </a>
+        <a href='https://www.awin1.com/cread.php?awinmid=22773&awinaffid=1166354&ued=https%3A%2F%2Fwww.sephora.co.uk%2F'  className='link'>
+        קוד הנחה בספורה בריטניה- MAYASPH 🛒
+        </a>
+        <a href='https://www.kamedis.co.il/'  className='link'>
+        קוד הנחה בקמדיס- MAYA10 🛒
+        </a>
+      </div>
     </div>
   );
 }
